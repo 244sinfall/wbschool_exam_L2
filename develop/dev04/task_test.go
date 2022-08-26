982f7b373469f0c400c6199c60d883d3cfe19f9b
Dima <dimafilin6@icloud.com>
2022-08-26 23:24:59 +0700

    task4 completed

--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,34 @@
 package main
+
+import "testing"
+
+func TestByteSumm(t *testing.T) {
+	word1 := "привет"
+	word2 := "тевирп"
+	if byteSumFor(word1) != byteSumFor(word2) {
+		t.Fail()
+	}
+	word3 := "прывет"
+	if byteSumFor(word1) == byteSumFor(word3) {
+		t.Fail()
+	}
+}
+
+func TestAnagrams(t *testing.T) {
+	var testArr = []string{"пятак", "пятка", "тяпка", "Капят", // Все ок
+		"монетка", "катеном", // Два элемента
+		"СЫР", // 1 элемент
+		"листок", "слиток", "столик"}
+	ang := showAnagrams(testArr)
+	for k := range ang {
+		byteSumFor(k)
+		for _, v := range ang[k] {
+			if byteSumFor(v) != byteSumFor(k) {
+				t.Fail()
+			}
+		}
+	}
+	if _, ok := ang["сыр"]; ok {
+		t.Fail()
+	}
+}
